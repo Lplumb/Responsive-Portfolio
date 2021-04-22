6c20e46f46a13fcec6d427c05c10620885f80737
Lucas Plumb <Lucasplumb@live.com>
2021-04-21 23:02:08 -0500

    updated portfolio for to include project 1 and some other projects, also made all the pages on one page that switch around the different info

---
+++ index.js
@@ -0,0 +1,86 @@
+
+//setting up click events
+$(".aboutMeClick").add(".portfolioClick").add(".contactClick").on("click", function(){
+    var navItem = $(this).text()
+
+    navItem = navItem.toLowerCase()
+
+    console.log(navItem)
+
+
+    displayStuff(navItem);
+
+})
+
+
+//sorting out using switch statements
+function displayStuff (navItem){
+    switch (navItem) {
+        case "about me":
+            console.log("test")
+            $(".aboutMe").attr("hidden", false)
+            $(".portfolio").attr("hidden", true)
+            $(".contact").attr("hidden", true)
+            displayAbout()
+            break;
+        case "portfolio":
+
+            $(".aboutMe").attr("hidden", true)
+            $(".portfolio").attr("hidden", false)
+            $(".contact").attr("hidden", true)
+            // console.log("other test")
+            displayPort()
+            break;
+        case "contact":
+            console.log("contact test")
+
+            $(".aboutMe").attr("hidden", true)
+            $(".portfolio").attr("hidden", true)
+            $(".contact").attr("hidden", false)
+            displayContact()
+            break;
+    }
+}
+
+
+function displayAbout (){
+
+    var aboutMeLabel = "About Me"
+
+    $(".label").text("")
+
+    $(".label").append(aboutMeLabel)
+}
+
+function displayPort() {
+
+    var portLabel = "Portfolio"
+
+    console.log("portfolio test")
+
+    $(".label").text("")
+
+    $(".label").append(portLabel)
+
+}
+
+function displayContact(){
+    var contactLabel = "Contact Me"
+
+    console.log("contect test")
+    
+    $(".label").text("")
+
+    $(".label").append(contactLabel)
+
+    
+}
+
+
+
+
+//Target H2 to change whats viewing
+//target a card on the index to switch info showing
+//makes images view properly or somthing i dunno
+//working links to projects that view need 3
+//
--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 This is a website that acts as a personal portfolio page for me. It contains an about me section, a portfolio section and a section to contact me thorught email.
 the website contains a nav bar to go between the pages and a footer that links to social media(s).
+https://lplumb.github.io/Responsive-Portfolio/index.html
+
+
+
+![](screenshot/images.jpg)
+![](screenshot/port.jpg)
+![](screenshot/contact.jpg)
